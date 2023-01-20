38c2cc2a24358da779e720131ecd9d0e4b81748b
quillaur <aurelienquillet@gmail.com>
2023-01-20 21:11:02 +0100

    Intermediate push.

--- data_science/machine_learning/reinforcement_learning/dqn/dqn_agent.py
+++ data_science/machine_learning/reinforcement_learning/dqn/dqn_agent.py
@@ -2,6 +2,11 @@
 
 import numpy as np
 import tensorflow as tf
+import gym
+import matplotlib.pyplot as plt
+from tqdm.notebook import trange
+
+from utils import VectorizedEnvWrapper, ReplayBuffer
 
 
 class Agent:
@@ -10,25 +15,29 @@ class Agent:
         self.epsilon_f = epsilon_f
         self.n_epsilon = n_epsilon
         self.epsilon = epsilon_i
-        self.gamma = gamma
+        self.discount_factor = gamma
+        self.learning_rate = alpha
 
         self.num_actions = num_actions
+        self.state_shape = state_shape
         self.num_envs = num_envs
 
-        self.Q = tf.keras.models.Sequential()
-        self.Q.add(tf.keras.layers.Input(shape=state_shape))
-        for size in hidden_sizes:
-            self.Q.add(tf.keras.layers.Dense(size, activation='relu', use_bias='false', kernel_initializer='he_uniform', dtype='float64'))
-        self.Q.add(tf.keras.layers.Dense(self.num_actions, activation="linear", use_bias='false', kernel_initializer='zeros', dtype='float64'))
+        self.Q = self.create_model()
         
         # target network
-        self.Q_ = tf.keras.models.Sequential()
-        self.Q_.add(tf.keras.layers.Input(shape=state_shape))
-        for size in hidden_sizes:
-            self.Q_.add(tf.keras.layers.Dense(size, activation='relu', use_bias='false', kernel_initializer='he_uniform', dtype='float64'))
-        self.Q_.add(tf.keras.layers.Dense(self.num_actions, activation="linear", use_bias='false', kernel_initializer='zeros', dtype='float64'))
-        
-        self.optimizer = tf.keras.optimizers.Adam(alpha)  
+        self.Q_ = self.create_model()
+
+        self.synchronize()
+
+    def create_model(self):
+        init = tf.keras.initializers.HeUniform()
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Dense(24, input_shape=self.state_shape, activation='relu', kernel_initializer=init))
+        model.add(tf.keras.layers.Dense(12, activation='relu', kernel_initializer=init))
+        model.add(tf.keras.layers.Dense(self.num_actions, activation='linear', kernel_initializer=init))
+        model.compile(loss=tf.keras.losses.Huber(), optimizer=tf.keras.optimizers.Adam(learning_rate=self.learning_rate), metrics=['mae'])
+
+        return model
     
     def synchronize(self):
         self.Q_.set_weights(self.Q.get_weights())
@@ -43,10 +52,62 @@ class Agent:
             self.epsilon_f, 
             self.epsilon_i - (n/self.n_epsilon)*(self.epsilon_i - self.epsilon_f))
 
-    def update(self, s_t, a_t, r_t, s_t_next, d_t):
-        with tf.GradientTape() as tape:
-            Q_next = tf.stop_gradient(tf.reduce_max(self.Q_(s_t_next), axis=1)) # note we use Q_ 
-            Q_pred = tf.reduce_sum(self.Q(s_t)*tf.one_hot(a_t, self.num_actions, dtype=tf.float64), axis=1)
-            loss = tf.reduce_mean(0.5*(r_t + (1-d_t)*self.gamma*Q_next - Q_pred)**2)
-        grads = tape.gradient(loss, self.Q.trainable_variables)
-        self.optimizer.apply_gradients(zip(grads, self.Q.trainable_variables))
+    def update(self, batches):
+        # in one batch is a list of: [state, action, reward, state_next, done]
+        X = []
+        y = []
+        for batch in batches:
+            
+            actual_states = [np.array(s[0]) for s in batch]
+            actual_preds = self.Q.predict(actual_states)
+            next_states = [np.array(s[3]) for s in batch]
+            target_preds = self.Q_.predict(next_states)
+
+            for index, (state, action, reward, next_state, trunc, done) in enumerate(batch):
+                if (not done) and (not trunc):
+                    max_future_q = reward + self.discount_factor * np.max(target_preds[index])
+                else:
+                    max_future_q = reward
+
+                current_qs = actual_preds[index]
+                current_qs[action] = (1 - self.learning_rate) * current_qs[action] + self.learning_rate * max_future_q
+
+                X.append(state)
+                y.append(current_qs)
+        
+        self.Q.fit(X, y, batch_size=32, verbose=0, shuffle=True)
+            
+
+
+
+def train(env_name, T=20000, num_envs=32, batch_size=32, sync_every=100, hidden_sizes=[24, 24], alpha=0.001, gamma=0.95):
+    env = VectorizedEnvWrapper(lambda: gym.make(env_name), num_envs)
+    state_shape = env.observation_space.shape
+    num_actions = env.action_space.n
+    agent = Agent(state_shape, num_actions, num_envs, alpha=alpha, hidden_sizes=hidden_sizes, gamma=gamma)
+    rewards = []
+    buffer = ReplayBuffer()
+    episode_rewards = 0
+    state = env.reset()
+    for t in trange(T):
+        if t%sync_every == 0:
+            agent.synchronize()
+        
+        action = agent.act(state)
+        state_next, reward, done, trunc, info = env.step(action)
+        buffer.remember(state, action, reward, state_next, trunc, done)
+        state = state_next
+
+        agent.update(buffer.sample(batch_size))
+        agent.decay_epsilon(t/T)
+        episode_rewards += reward
+
+        for i in range(env.num_envs):
+            if done[i] or trunc[i]:
+                rewards.append(episode_rewards[i])
+                episode_rewards[i] = 0
+                state[i] = env.reset_at(i)
+            
+    # plot(pd.DataFrame(rewards), window=10)
+    plt.plot(rewards)
+    return agent
